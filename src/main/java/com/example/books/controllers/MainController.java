99c5bfebdcbe29f7e6527f9f1faaf6b78c838b9f
Dominic Basa <steelersfan2990@gmail.com>
2023-02-15 23:17:48 -0600

    updated

--- src/main/java/com/example/books/controllers/MainController.java
+++ src/main/java/com/example/books/controllers/MainController.java
@@ -1,8 +1,10 @@
 package com.example.books.controllers;
 
+import com.example.books.models.Book;
+import java.util.ArrayList;
 import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+
+  ArrayList<Book> books = new ArrayList<>();
 
   //? CREATE
 
@@ -22,9 +26,13 @@ public class MainController {
   public String createBook(
     @RequestParam("title") String title,
     @RequestParam("author") String author,
-    @RequestParam("pages") Integer pages, HttpSession session
+    @RequestParam("pages") Integer pages,
+    // @RequestParam("description") String description,
+    HttpSession session
   ) {
-    System.out.println(title);
+    Book book = new Book(title, author, pages);
+    System.out.println(book.getTitle());
+    books.add(book);
     session.setAttribute("title", title);
     session.setAttribute("author", author);
     session.setAttribute("pages", pages);
@@ -34,12 +42,27 @@ public class MainController {
   //? READ ALL
 
   @RequestMapping("/")
-  public String index() {
+  public String index(Model model) {
+    model.addAttribute("books", books);
     return "index.jsp";
   }
+  
+  
   //? READ ONE
-
+  @RequestMapping("/books/1")
+  public String show(Model model) {
+    model.addAttribute("books", books.get(1));
+    model.addAttribute("books", books);
+    return "show.jsp";
+  }
+  
+  
+  
   //? UPDATE
 
+  
+  
+  
+  
   //? DELETE
 }
--- src/main/webapp/WEB-INF/index.jsp
+++ src/main/webapp/WEB-INF/index.jsp
@@ -19,9 +19,18 @@
     <script type="text/javascript" src="/js/app.js"></script><!-- change to match your file/naming structure -->
 </head>
 <body>
-  <p>Title <c:out value="${title}"></c:out></p>
+  <table class="table">
+    <c:forEach var="book" items="${books}">
+      <tr>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>
+      </tr>
+    </c:forEach>
+  </table>
+  <!-- <p>Title <c:out value="${title}"></c:out></p>
   <p>Author <c:out value="${author}"></c:out></p>
-  <p>Pages <c:out value="${pages}"></c:out></p>
+  <p>Pages <c:out value="${pages}"></c:out></p> -->
   <a class="btn btn-primary" href="/books/new"> Add a book</a>
 </body>
 </html>
